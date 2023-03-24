f6e773341c311df61546525e5398ff8157ecbfcb
Sebi <Sebi@DESKTOP-4OIT45V>
2023-03-24 16:25:54 +0200

    Add project files.

---
+++ TokenRing/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenRing
+{
+    public class Calculator
+    {
+
+        public String Ip { get; set; }
+        public Calculator Next { get; set; }
+        public Calculator Prev { get; set; }
+
+        public Token Token { get; set; }
+        public Calculator(String ip)
+        {
+            Ip = ip;
+            Next = null;
+            Prev = null;
+            Token = null;
+        }
+
+        
+
+    }
+}
---
+++ TokenRing/Token.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenRing
+{
+    public class Token
+    {
+        public String IpSursa { get; set; }
+
+        public String IpDestinatie { get; set; }
+
+        public String Mesaj { get; set; }
+
+        public bool AjunsLaDestinatie { get; set; }
+
+        public bool Liber { get; set; }
+
+        List<Calculator> Istoric {  get; set; }
+       
+    }
+}
